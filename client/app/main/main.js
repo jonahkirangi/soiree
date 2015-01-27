c179ad649e42db1a65af0fdd52f432e15ddc0817
Jonah Kirangi <jonahkirangi@gmail.com>
2015-01-26 19:47:39 -0800

    first commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('soireeApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
