792cecb9e0ccded2522cfe302cf0272c0000d709
Jonah Kirangi <jonahkirangi@gmail.com>
2015-01-27 23:18:21 -0800

    Add socketMock module

--- client/app/main/main.controller.spec.js
+++ client/app/main/main.controller.spec.js
@@ -3,7 +3,7 @@
 describe('Controller: MainCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('soireeApp'));
+  beforeEach(module('soireeApp', 'socketMock'));
 
   var MainCtrl, scope;
 
